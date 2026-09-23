b47cd7392affba828581531469d4a6b013fdc0d7
Africha Sekar Wangi <africhasekar@gmail.com>
2026-09-23 17:50:09 +0700

    Labwork 01 - Unguided

--- src/lw01/unguided/Main.java
+++ src/lw01/unguided/Main.java
@@ -1,39 +1,33 @@
 package lw01.unguided;
-import java.io.File;
-import java.io.FileNotFoundException;
+
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        try {
-            Scanner scanner = new Scanner(Main.class.getResourceAsStream("washes.txt"));
+        Scanner scanner = new Scanner(Main.class.getResourceAsStream("washes.txt"));
 
-            int total = scanner.nextInt();
-            WashService[] services = new WashService[total];
+        int total = scanner.nextInt();
+        WashService[] services = new WashService[total];
 
-            for (int i = 0; i < total; i++) {
-                String type = scanner.next();
-                String id = scanner.next();
-                int days = scanner.nextInt();
-                int units = scanner.nextInt();
+        for (int i = 0; i < total; i++) {
+            String type = scanner.next();
+            String id = scanner.next();
+            int days = scanner.nextInt();
+            int units = scanner.nextInt();
 
-                if (type.equals("MOTORCYCLE")) {
-                    services[i] = new MotorcycleWash(id, days, units);
-                } else if (type.equals("CAR")) {
-                    services[i] = new CarWash(id, days, units);
-                }
+            if (type.equals("MOTORCYCLE")) {
+                services[i] = new MotorcycleWash(id, days, units);
+            } else if (type.equals("CAR")) {
+                services[i] = new CarWash(id, days, units);
             }
+        }
 
-            scanner.close();
+        scanner.close();
 
-            for (WashService service : services) {
-                System.out.println(service.summary());
-            }
-
-        } catch (FileNotFoundException e) {
-            System.out.println("washes.txt not found.");
+        for (WashService service : services) {
+            System.out.println(service.summary());
         }
     }
 }
